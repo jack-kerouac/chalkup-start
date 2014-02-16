64416a2e77b39fe1109755d92519c419355825f6
Florian Rampp <Florian.Rampp@gmail.com>
2014-02-16 21:29:37 +0100

    readded own content

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,26 @@
 'use strict';
 
 angular.module('chalkupStartApp', []);
+
+
+
+// TODO: where to put the following?
+
+(function () {
+
+    //foundation init
+    $(document).foundation();
+
+    $('.flexslider').flexslider({
+        animation: "slide"
+    });
+
+    $('#notify form').submit(function (event) {
+        var dimensionValue = 'signedUp';
+
+        ga('set', 'dimension1', dimensionValue);
+
+        event.preventDefault();
+    });
+
+})();
