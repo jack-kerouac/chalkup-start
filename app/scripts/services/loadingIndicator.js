d23d3261cbd41df4e2c1c606eda8fe86bb2c1561
Florian Rampp <Florian.Rampp@gmail.com>
2014-02-25 11:48:49 +0100

    loading indicator now stops upon error
    and alerts about error

--- app/scripts/services/loadingIndicator.js
+++ app/scripts/services/loadingIndicator.js
@@ -3,16 +3,24 @@
 angular.module('chalkupStartApp')
     .service('LoadingIndicator', function LoadingIndicator($rootScope) {
         var waitQueue = [];
+
+        function removeFromQueue(promise) {
+            // remove this promise
+            var index = waitQueue.indexOf(promise);
+            waitQueue.splice(index, 1);
+            if (_.isEmpty(waitQueue))
+                $rootScope.loading = false;
+        }
+
         this.waitFor = function (promise) {
             $rootScope.loading = true;
 
             waitQueue.push(promise);
-            promise.then(function () {
-                // remove this promise
-                var index = waitQueue.indexOf(promise);
-                waitQueue.splice(index, 1);
-                if (_.isEmpty(waitQueue))
-                    $rootScope.loading = false;
+            promise.then(function (data) {
+                removeFromQueue(promise);
+            }, function (data) {
+                removeFromQueue(promise);
+                alert(data.config.method + " to " + data.config.url + " returned with " + data.status);
             });
         }
     });
