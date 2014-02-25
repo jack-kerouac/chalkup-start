14ce265a2ec66ea03984889d774aadc231aa90ad
Florian Rampp <Florian.Rampp@gmail.com>
2014-02-25 11:06:04 +0100

    minor

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,8 @@
 'use strict';
 
-angular.module('chalkupStartApp', ['ui.router', 'restangular', 'angularSpinner', 'angularMoment'])
+angular.module('chalkupStartApp', ['ui.router', 'restangular', 'angularSpinner', 'angularMoment']);
+
+angular.module('chalkupStartApp')
     .run(function ($window, $rootScope, $state, userService) {
         // foundation init when displaying view/include
         $rootScope.$on('$viewContentLoaded', function () {
