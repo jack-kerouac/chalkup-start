d97c71ab79957f7f7be86c61dd913bdafcee9e1e
Florian Rampp <Florian.Rampp@gmail.com>
2014-03-10 19:50:17 +0100

    added visualization of 90 percentile in graph

--- app/scripts/directives/gradeChart.js
+++ app/scripts/directives/gradeChart.js
@@ -12,6 +12,7 @@ angular.module('chalkupStartApp')
             link: function ($scope, elem) {
                 var foregroundColor = 'white';
                 var gridColor = '#0D4F7A';
+                var ninetyPercentileOpacity = 0.3;
 
                 $scope.options = {
                     xaxis: {
@@ -66,13 +67,19 @@ angular.module('chalkupStartApp')
                         if(_.isEmpty(gradeData))
                             return;
 
+                        // TODO: remove this as soon as we have real stddev data from the server
+                        for(var i = 0; i < gradeData.length; i++)
+                            gradeData[i].push(0.05 * 1 / (i + 1));
+
                         var minGrade = _.min(_.map(gradeData, function(point) { return point[1]}));
                         var maxGrade = _.max(_.map(gradeData, function(point) { return point[1]}));
 
                         $scope.options.yaxes[0].min = minGrade - gradeDiff * 1.2;
                         $scope.options.yaxes[0].max = maxGrade + gradeDiff * 1.2;
 
-                        var chartData = [{ data: gradeData, color: foregroundColor, yaxis: 1 }];
+                        var ninetyPercentileAreaConf = [{ representation: 'symmetric', opacity: ninetyPercentileOpacity }];
+
+                        var chartData = [{ data: gradeData, fillArea: ninetyPercentileAreaConf, color: foregroundColor, yaxis: 1 }];
                         chart = $.plot(elem, chartData, $scope.options);
                     });
                 });
